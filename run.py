39241e5aa1f3fb9c6a2007a32f5608b35150bf94
Yohan <yohan.yudhistira90@gmail.com>
2021-04-30 20:45:09 +0700

    rei backpacks to csv

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ from requests_html import HTMLSession
 import chompjs
 import pandas as pd
 
+
 def fetch(x):
     base_url = 'https://www.rei.com'
     r = s.get(f'https://www.rei.com/c/hiking-backpacks?page={x}')
     results = [base_url + link.attrs['href'] for link in r.html.find('#search-results > ul > li > a')]
     return list(dict.fromkeys(results))
+
 
 def parse_product(url):
     r = s.get(url)
@@ -14,8 +16,9 @@ def parse_product(url):
     data = chompjs.parse_js_object(details.text)
     return data
 
+
 def main():
-    products = [url for x in range(1,3) for url in fetch(x)]
+    products = [url for x in range(1, 3) for url in fetch(x)]
     return [parse_product(url) for url in products]
 
 
@@ -24,5 +27,3 @@ if __name__ == '__main__':
     df = pd.json_normalize(main())
     df.to_csv('rei-backpacks.csv', index=False)
     print('finished')
-
-
